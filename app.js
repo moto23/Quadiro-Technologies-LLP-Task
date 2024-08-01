44675a977cba132d72f3393ac3fac06e0b069d11
Prasad Nathe <99172728+moto23@users.noreply.github.com>
2024-08-02 01:46:56 +0530

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/cars', carRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
